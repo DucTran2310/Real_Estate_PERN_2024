06ed9d2e3e143c71d3b80625a80945dde9c69d75
DucTran2310 <trananhduc23102000@gmail.com>
2024-05-28 16:05:47 +0700

    Verify phone number by OTP firebase

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,155 +1,222 @@
-import { apiRegister, apiSignIn } from "@apis/auth"
-import InputForm from "@components/inputs/InputForm"
-import { useAppStore } from "@store/useAppStore"
-import { SIGN_IN } from "@utils/constants"
-import clsx from "clsx"
-import { useEffect, useState } from "react"
-import { useForm } from "react-hook-form"
-import { toast } from "react-toastify"
-import Swal from 'sweetalert2'
-import { Button, InputRadio } from ".."
-import Loading from "@components/loading/Loading"
-import { useUserStore } from "@store/useUserStore"
+/* eslint-disable react-hooks/rules-of-hooks */
+import clsx from "clsx";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { toast } from "react-toastify";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import { twMerge } from "tailwind-merge";
+import { useAppStore } from "@store/useAppStore";
+import { useUserStore } from "@store/useUserStore";
+import auth from "@utils/firebase.config";
+import { apiRegister, apiSignIn } from "@apis/auth";
+import { Button, InputForm, InputRadio, OtpVerifier } from "..";
+import useWithRouter from "@hooks/useWithRouter";
 
-const Login = () => {
+const Login = ({ navigate }) => {
+  const [variant, setVariant] = useState("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+  const { setModal } = useAppStore();
+  const [isShowConfirmOTP, setIsShowConfirmOTP] = useState(false);
+  const { setToken, listRoles } = useUserStore();
 
-  const [variant, setVariant] = useState(SIGN_IN.LOGIN)
-  const { register, formState: { errors }, handleSubmit, reset } = useForm()
-  const { setModal, setLoading, isLoading } = useAppStore()
-  const { setToken, listRoles } = useUserStore()
+  console.log('VVVroles: ', listRoles)
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm()
 
   useEffect(() => {
     reset()
-  }, [reset, variant])
+  }, [variant])
 
-  const onSubmit = async (data) => {
-    if (variant === SIGN_IN.REGISTER) {
-      setLoading(true)
-      const response = await apiRegister(data)
-      setLoading(false)
-      if (response.success) {
-        Swal.fire({
-          icon: "success",
-          title: "Congratulation",
-          text: response.toastMessage,
-          showConfirmButton: true,
-          confirmButtonText: "Go Sign In",
-        }).then(({ isConfirmed }) => {
-          if (isConfirmed) {
-            setVariant(SIGN_IN.LOGIN)
-          }
-        });
-      } else {
-        toast.error(response.toastMessage)
-      }
-    }
-
-    if (variant === SIGN_IN.LOGIN) {
-      setLoading(true)
-      const response = await apiSignIn(data)
-      setLoading(false)
-      if (response.success) {
-        toast.success(response?.toastMessage)
-        setToken(response.accessToken)
-        setModal(false, null)
-      } else {
-        toast.error(response.toastMessage)
-      }
+  // Create captcha if have not exist!!!
+  const handleCaptchaVerify = () => {
+    if (!window.recaptchVerify) {
+      window.recaptchVerify = new RecaptchaVerifier(
+        auth,
+        "recaptcha-verifier",
+        {
+          size: "invisible",
+          callback: () => {},
+          "expired-callback": () => {}
+        }
+      );
     }
   }
 
-  return (
+  const handleSendOTP = (phone) => {
+    setIsLoading(true)
+    handleCaptchaVerify()
+    const verifier = window.recaptchVerify
+    const formatPhone = "+84" + phone.slice(1)
+    signInWithPhoneNumber(auth, formatPhone, verifier)
+      .then((result) => {
+        setIsLoading(false)
+        window.confirmationResult = result
+        toast.success("Sent OTP your phone")
+        setIsShowConfirmOTP(true)
+      })
+      .catch((error) => {
+        setIsLoading(false)
+        window.isSentOTP = false
+        toast.error("Something went wrong")
+        console.log('VVVERROR: ', error)
+      })
+  }
 
-    isLoading
-      ? <Loading />
-      :
-      <div
-        onClick={(e) => e.stopPropagation()}
-        className="bg-white text-lg rounded-md px-6 py-8 w-[500px] flex flex-col items-center gap-6"
-      >
-        <h1 className="text-3xl font-semibold font-montserrat tracking-tighter">
-          Welcome Real estate
-        </h1>
-        <div className="flex border-b w-full justify-start gap-6">
-          <span
-            onClick={() => setVariant(SIGN_IN.LOGIN)}
-            className={clsx(variant === SIGN_IN.LOGIN && 'border-b-4 border-main-700', "cursor-pointer")}
-          >
-            Login
-          </span>
-          <span
-            onClick={() => setVariant(SIGN_IN.REGISTER)}
-            className={clsx(variant === SIGN_IN.REGISTER && 'border-b-4 cursor-pointer border-main-700', "cursor-pointer")}
-          >
-            New account
-          </span>
+  const onSubmit = async (data) => {
+    if (variant === "REGISTER") {
+      if (data?.roleCode !== "ROL7") {
+        handleSendOTP(data.phone)
+      } else handleRegister(data)
+    }
+    if (variant === "LOGIN") {
+      const { name, role, ...payload } = data
+      const response = await apiSignIn(payload)
+      if (response.success) {
+        toast.success(response.toastMessage)
+        setToken(response.accessToken)
+        setModal(false, null)
+      } else toast.error(response.toastMessage)
+    }
+  }
+
+  const handleRegister = async (data) => {
+    const response = await apiRegister(data)
+    if (response.success) {
+      Swal.fire({
+        icon: "success",
+        title: "Congratulation",
+        text: response.mes,
+        showConfirmButton: true,
+        confirmButtonText: "Go Sign In",
+      }).then(({ isConfirmed }) => {
+        if (isConfirmed) {
+          setVariant("LOGIN")
+          setIsShowConfirmOTP(false)
+        }
+      })
+    } else toast.error(response.toastMessage)
+  }
+
+  return (
+    <div
+      onClick={(e) => e.stopPropagation()}
+      className={twMerge(
+        clsx(
+          "bg-white relative text-base rounded-md px-6 py-8 w-[500px] items-center flex flex-col gap-6",
+          isShowConfirmOTP && "w-[600px] h-[270px]"
+        )
+      )}
+    >
+      {isShowConfirmOTP && (
+        <div className="absolute inset-0 bg-white rounded-md">
+          <OtpVerifier cb={handleSubmit(handleRegister)} />
         </div>
-        <form className="w-full flex flex-col gap-4">
+      )}
+      <h1 className="text-3xl font-Agbalumo font-semibold tracking-tight">
+        Welcome to Soul Rest
+      </h1>
+      <div
+        className={twMerge(
+          clsx(
+            "flex border-b w-full justify-start  gap-6",
+            isShowConfirmOTP && "hidden"
+          )
+        )}
+      >
+        <span
+          onClick={() => setVariant("LOGIN")}
+          className={clsx(
+            variant === "LOGIN" && "border-b-4  border-main-700",
+            "cursor-pointer"
+          )}
+        >
+          Login
+        </span>
+        <div id="recaptcha-verifier"></div>
+        <span
+          onClick={() => setVariant("REGISTER")}
+          className={clsx(
+            variant === "REGISTER" && "border-b-4  border-main-700",
+            "cursor-pointer"
+          )}
+        >
+          New account
+        </span>
+      </div>
+      <form
+        className={twMerge(
+          clsx("flex w-full px-4 flex-col gap-4", isShowConfirmOTP && "hidden")
+        )}
+      >
+        <InputForm
+          label="Phone Number"
+          inputClassname="rounded-md"
+          register={register}
+          id="phone"
+          placeholder="Type your phone number"
+          validate={{
+            required: "This field cannot empty.",
+            // pattern: {
+            //   value: /(|0[1|3|5|7|8|9])+([0-9]{8})\b/,
+            //   message: "Phone number invalid",
+            // },
+          }}
+          errors={errors}
+        />
+        <InputForm
+          label="Password"
+          inputClassname="rounded-md"
+          register={register}
+          id="password"
+          placeholder="Type your password"
+          validate={{ required: "This field cannot empty." }}
+          type="password"
+          errors={errors}
+        />
+        {variant === "REGISTER" && (
           <InputForm
-            label='Phone number'
+            label="Your fullname"
             inputClassname="rounded-md"
             register={register}
-            type="number"
-            id='phone'
-            placeholder="Type your phone number here"
-            validate={{
-              required: "This field cannot empty.",
-              pattern: {
-                value: /(|0[1|3|5|7|8|9])+([0-9]{8})\b/,
-                message: "Phone number invalid",
-              },
-            }}
-            errors={errors}
-          />
-          <InputForm
-            label='Password'
-            inputClassname="rounded-md"
-            register={register}
-            id='password'
-            placeholder="Type your password here"
-            type="password"
+            id="name"
+            placeholder="Type your name"
             validate={{ required: "This field cannot empty." }}
             errors={errors}
           />
-          {
-            variant === SIGN_IN.REGISTER && (
-              <InputForm
-                label='Your Fullname'
-                inputClassname="rounded-md"
-                register={register}
-                id='name'
-                placeholder="Type your fullname here"
-                validate={{ required: "This field cannot empty." }}
-                errors={errors}
-              />
-            )
-          }
-          {variant === "REGISTER" && (
-            <InputRadio
-              label="Type account"
-              register={register}
-              id="roleCode"
-              validate={{ required: "This field cannot empty." }}
-              optionClassname="grid grid-cols-3 gap-4"
-              errors={errors}
-              options={listRoles
-                ?.filter(el => el.code !== 'ROL1')
-                ?.map(el => ({ label: el.value, value: el.code }))}
-            />
-          )}
-          <Button
-            className="py-2 my-6"
-            handleOnClick={handleSubmit(onSubmit)}
-          >
-            {variant === SIGN_IN.LOGIN ? SIGN_IN.LOGIN : SIGN_IN.REGISTER}
-          </Button>
-          <span className="cursor-pointer text-main-500 hover:underline w-full text-center">
-            Forgot your password?
-          </span>
-        </form>
-      </div>
+        )}
+        {variant === "REGISTER" && (
+          <InputRadio
+            label="Type account"
+            register={register}
+            id="roleCode"
+            validate={{ required: "This field cannot empty." }}
+            optionClassname="grid grid-cols-3 gap-4"
+            errors={errors}
+            options={listRoles
+              ?.filter((el) => el.code !== "ROL1")
+              .map((el) => ({ label: el.value, value: el.code }))}
+          />
+        )}
 
-  )
-}
+        <Button
+          disabled={isLoading}
+          handleOnClick={handleSubmit(onSubmit)}
+          className="py-2 my-6"
+        >
+          {variant === "LOGIN" ? "Sign In" : "Register"}
+        </Button>
+        <span className="cursor-pointer text-main-500 hover:underline w-full text-center">
+          Forgot your password
+        </span>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default useWithRouter(Login)
